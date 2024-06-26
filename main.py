d7e3c5a93b2ffe9dace3f527119ffc9b059ff799
Chen6528 <Jamespichen2005@gmail.com>
2024-06-26 14:55:57 -0400

    update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import mysql.connector
 import pandas as pd
 import getpass
+import tkinter as tk
+from tkinter import ttk
+
 
 user = getpass.getpass(prompt='User: ')
 password = getpass.getpass(prompt='Password: ')
@@ -9,12 +12,22 @@ connection = mysql.connector.connect(
     user=user,
     password=password,
     host="localhost",
-    database="test_import"
+    database="player_info"
     )
 cursor = connection.cursor()
 
 query = ''' 
-        
+        SELECT player_name, age, nation, team, market_value 
+        FROM player_stats 
+        ORDER BY market_value DESC 
+        LIMIT 100
         '''
-
 cursor.execute(query)
+
+rows = cursor.fetchall()
+
+cursor.close()
+connection.close()
+df = pd.DataFrame(rows)
+
+
