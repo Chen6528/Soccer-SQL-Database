76eece2678b70f8810333f4b470aa9d574be7441
Chen6528 <Jamespichen2005@gmail.com>
2024-06-25 23:44:47 -0400

    created table in mySQL & loaded from csv file

--- MySqL Local.session.sql
+++ MySqL Local.session.sql
@@ -1,4 +1,19 @@
 create DATABASE player_info;
 
 -- @block
-show DATABASES
+USE player_info
+
+-- @block
+CREATE TABLE player_stats (
+    player_name VARCHAR(255),
+    age INT,
+    position VARCHAR(255),
+    nation VARCHAR(255),
+    team VARCHAR(255),
+    market_value FLOAT
+)
+
+
+
+-- @block
+SELECT * from player_stats
